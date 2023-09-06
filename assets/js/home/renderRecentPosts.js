d1da6f31e3e7c161cf596d74a08976e07ed68569
Ícaro Cedraz <icarocedraz7@gmail.com>
2023-09-06 18:11:55 -0300

    Update renderRecentPosts.js

--- assets/js/home/renderRecentPosts.js
+++ assets/js/home/renderRecentPosts.js
@@ -82,8 +82,7 @@ function smallPost(
 async function renderPosts() {
   try {
     const posts = await getPosts();
-    console.log(posts);
-    const slicePosts = posts.slice(-6, posts.length);
+    const slicePosts = posts.slice(-6);
     slicePosts.reverse();
 
     if (slicePosts.length == 1) {
